52550edd68afefa1c94008c150cf61a1d936ea21
Markus Heck <markus.heck@hs-kempten.de>
2024-11-15 00:15:39 +0100

    update method docs

--- classes/local/moodle_core.php
+++ classes/local/moodle_core.php
@@ -11,42 +11,50 @@ use stdClass;
  * This class contains aliases for moodle core functions to allow mocking them.
  */
 class moodle_core {
-    /** alias for get_all_roles() */
+    /** alias for {@link get_all_roles()} */
     public static function get_all_roles(...$args): array {
         return get_all_roles(...$args);
     }
 
-    /** alias for create_role()
+    /** alias for {@link create_role()}
      * @throws coding_exception
      */
     public static function create_role(...$args): int {
         return create_role(...$args);
     }
 
-    /** alias for role_exists()
+    /** alias for {@link assign_capability()}
+     *
+     * context id: https://docs.moodle.org/405/en/Override_permissions
+     *
      * @throws coding_exception
      */
     public static function assign_capability(...$args): bool {
         return assign_capability(...$args);
     }
 
-    /**
+    /** alias for {@link unassign_capability()}
      * @throws coding_exception
      */
     public static function unassign_capability(string $capability, int $role_id, int|context $contextid = null): bool {
         return unassign_capability($capability, $role_id);
     }
 
-    /** alias for get_role_contextlevels() */
+    /** alias for {@link get_role_contextlevels()} */
     public static function get_role_contextlevels(...$args): array {
         return get_role_contextlevels(...$args);
     }
 
-    /** alias for context_coursecat::instance() */
+    /** alias for {@link context_coursecat::instance()} */
     public static function context_coursecat_instance(...$args): object {
         return context_coursecat::instance(...$args);
     }
 
+    /** function that filters {@link get_all_roles()} for a specific role
+     *
+     * @param string $role_shortname
+     * @return stdClass|false
+     */
     public static function get_role(string $role_shortname): stdClass|false {
         foreach (self::get_all_roles() as $role) {
             if ($role->shortname == $role_shortname) {
