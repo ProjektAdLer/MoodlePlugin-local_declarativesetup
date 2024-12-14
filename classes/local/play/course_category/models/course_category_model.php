c8b4692bef59f724a54a9449f4f2929341d2e39a
Markus Heck <markus.heck@hs-kempten.de>
2024-12-14 21:49:51 +0100

    refactor model

--- classes/local/play/course_category/models/course_category_model.php
+++ classes/local/play/course_category/models/course_category_model.php
@@ -6,31 +6,20 @@ use local_declarativesetup\local\play\course_category\util\course_category_path;
 
 class course_category_model {
     /**
-     * @param string $course_category_path see {@link $course_category_path}
-     * @param bool $present see {@link $present}
-     * @param bool $append_users see {@link $append_users}
-     * @param role_user_model[] $users see {@link $users}
-     * @param string $description see {@link $description}, default: "created by declarativesetup"
+     * @param string $course_category_path
+     * @param bool $present true: course category should be present (create/update), false: user should be absent (delete)
+     * @param bool $append_users if true, users will be added to existing roles, if false, existing roles will be replaced
+     * @param role_user_model[] $users
+     * @param string $description
      */
     public function __construct(string $course_category_path,
-                                bool   $present = true,
-                                bool   $append_users = true,
-                                array  $users = [],
-                                string $description = "created by declarativesetup") {
+                                public bool   $present = true,
+                                public bool   $append_users = true,
+                                public array  $users = [],
+                                public string $description = "created by declarativesetup") {
 
         $this->course_category_path = new course_category_path($course_category_path);
-        $this->present = $present;
-        $this->append_users = $append_users;
-        $this->users = $users;
-        $this->description = $description;
     }
 
     public course_category_path $course_category_path;
-    /** @var bool true: course category should be present (create/update), false: user should be absent (delete) */
-    public bool $present;
-    /** @var bool if true, users will be added to existing roles, if false, existing roles will be replaced */
-    public bool $append_users;
-    /** @var role_user_model[] */
-    public array $users;
-    public string $description;
 }
--- classes/local/play/course_category/models/role_user_model.php
+++ classes/local/play/course_category/models/role_user_model.php
@@ -4,22 +4,13 @@ namespace local_declarativesetup\local\play\course_category\models;
 
 class role_user_model {
     /**
-     * @param string $username see {@link $username}
-     * @param array $roles see {@link $roles}
-     * @param bool $append_roles see {@link $append_roles}
+     * @param string $username shortname
+     * @param string[] $roles shortname
+     * @param bool $append_roles if true, roles will be added to existing roles, if false, existing roles will be replaced
      */
     public function __construct(string $username, array $roles, bool $append_roles = true) {
         $this->username = $username;
         $this->roles = $roles;
         $this->append_roles = $append_roles;
     }
-
-    /** @var string shortname */
-    public string $username;
-    /** @var string[] role shortname */
-    public array $roles;
-    /** @var bool if true, roles will be added to existing roles, if false, existing roles will be replaced */
-    public bool $append_roles;
-    /** @var bool true: user should be present (create/update), false: user should be absent (delete) */
-    public bool $present;
 }
