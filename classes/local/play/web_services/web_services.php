831a43be73f219615054aa222b7d26158722d75a
Markus Heck <markus.heck@hs-kempten.de>
2024-12-15 05:40:37 +0100

    fix moodle datatypes...

--- classes/local/play/web_services/web_services.php
+++ classes/local/play/web_services/web_services.php
@@ -109,13 +109,15 @@ class web_services extends base_play {
     private function update_mobile_service(): bool {
         if ($this->input->enable_moodle_mobile_service !== web_services_model::STATE_UNSET) {
             $external_service_record = di::get(moodle_external_services_repository::class)->get_external_service_by_shortname(MOODLE_OFFICIAL_MOBILE_SERVICE);
-            if ($external_service_record->enabled === 1 && $this->input->enable_moodle_mobile_service === web_services_model::STATE_DISABLED) {
+            if ((int)$external_service_record->enabled === 1 && $this->input->enable_moodle_mobile_service === web_services_model::STATE_DISABLED) {
                 $external_service_record->enabled = 0;
                 di::get(moodle_external_services_repository::class)->update_external_service($external_service_record->id, $external_service_record);
+                cli_writeln("MOODLE_OFFICIAL_MOBILE_SERVICE were enabled and are now disabled");
                 return true;
-            } elseif ($external_service_record->enabled === 0 && $this->input->enable_moodle_mobile_service === web_services_model::STATE_ENABLED) {
+            } elseif ((int)$external_service_record->enabled === 0 && $this->input->enable_moodle_mobile_service === web_services_model::STATE_ENABLED) {
                 $external_service_record->enabled = 1;
                 di::get(moodle_external_services_repository::class)->update_external_service($external_service_record->id, $external_service_record);
+                cli_writeln("MOODLE_OFFICIAL_MOBILE_SERVICE were disabled and are now enabled");
                 return true;
             }
         }
