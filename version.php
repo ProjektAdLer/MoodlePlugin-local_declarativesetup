8116ff031fa678413bb90d915ac029c0a597b4f3
Markus Heck <markus.heck@hs-kempten.de>
2025-03-17 00:51:30 +0100

    bump version.php

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_declarativesetup';
-$plugin->release = '1.0.1-dev';
-$plugin->version = 2025031600;
+$plugin->release = '1.1.1';
+$plugin->version = 2025031602;
 $plugin->requires = 2024042200;
 $plugin->maturity = MATURITY_STABLE;
